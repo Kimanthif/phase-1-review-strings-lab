3b11cbb1bab122e4dc7466d4705d22e62734604e
Felistus Kimanthi <kimanthi.felistus@gmail.com>
2025-03-31 07:14:38 +0300

    greetings and current user defined

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+
+let currentUser = "Felistus Kimanthi";
+
+//currentUser = "Felistus Kimanthi";
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
